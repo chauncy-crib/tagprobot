8896dc20e7f2c9daf622087976e40313bdb4e3ed
Bill Wong <bill@cadre.com>
2017-07-16 13:03:08 -0400

    added gameState.js

--- src/helpers/finders.js
+++ src/helpers/finders.js
@@ -1,13 +1,19 @@
 import _ from 'lodash';
 
 import { tileTypes } from '../constants';
-import { getMyEndzoneTile, isOnMyTeam } from './player';
+import { getMyEndzoneTile, getEnemyEndzoneTile, isOnMyTeam } from './player';
 import { findTile, findApproxTile } from './map';
 
 // Returns the position of the endzone you should return a the flag to.
 // TODO: return closest endzone tile instead of first
 export function findMyEndzone() {
   return findTile(getMyEndzoneTile());
+}
+
+// Returns the position of the endzone you should defend
+// TODO: return closest endzone tile instead of first
+export function findEnemyEndzone() {
+  return findTile(getEnemyEndzoneTile());
 }
 
 /*
