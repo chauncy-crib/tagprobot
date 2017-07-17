13631c5d48c6e9511f3bd800c538bc245551cf86
David Abrahams <david.abrahams@students.olin.edu>
2017-07-16 17:02:09 -0700

    remove the function findApxTile, make findTile take in a list of tiles to match on

--- src/helpers/finders.js
+++ src/helpers/finders.js
@@ -2,25 +2,25 @@ import _ from 'lodash';
 
 import { tileTypes } from '../constants';
 import { getMyEndzoneTile, getEnemyEndzoneTile, isOnMyTeam } from './player';
-import { findTile, findApproxTile } from './map';
+import { findTile } from './map';
 
 // Returns the position of the endzone you should return a the flag to.
 // TODO: return closest endzone tile instead of first
 export function findMyEndzone() {
-  return findTile(getMyEndzoneTile());
+  return findTile([getMyEndzoneTile()]);
 }
 
 // Returns the position of the endzone you should defend
 // TODO: return closest endzone tile instead of first
 export function findEnemyEndzone() {
-  return findTile(getEnemyEndzoneTile());
+  return findTile([getEnemyEndzoneTile()]);
 }
 
 /*
  * Returns the position (in pixels) of the specified flag station, even if empty.
  */
 export function findFlagStation() {
-  return findApproxTile(tileTypes.YELLOW_FLAG);
+  return findApproxTile([tileTypes.YELLOW_FLAG, tileTypes.YELLOW_FLAG_TAKEN]);
 }
 
 // Returns the enemy FC if in view.
