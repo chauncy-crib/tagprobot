33d2bca778e60594b9742741c40e51ebb4af125c
David Abrahams <david.abrahams@students.olin.edu>
2017-07-22 01:25:15 -0700

    this passes npm run build:test

--- src/helpers/map.js
+++ src/helpers/map.js
@@ -137,20 +137,19 @@ export function getTraversableCells(cpt, map) {
   }
   for (x = 0; x < xl; x++) {
     for (let y = 0; y < yl; y++) {
-      // TODO: Set radius to be correct value for each cell object. Currently using 29 because it is > 20 * sqrt(2),
-      // the furthest distance from the center of a tile to its corner, in pixels. This guarantees that if the tile
-      // has anything non-tranversable in it (wall, ball, spike, corner wall), the entire tile is marked as non-traversable
+      // TODO: Set radius to be correct value for each cell object. Currently using 29 because it
+      // is > 20 * sqrt(2), the furthest distance from the center of a tile to its corner, in
+      // pixels. This guarantees that if the tile has anything non-tranversable in it (wall, ball,
+      // spike, corner wall), the entire tile is marked as non-traversable
       const objRadius = 29;
       fillGridWithSubgrid(
         emptyCells,
         traversableCellsInTile(
           isTraversable(map[x][y]),
           cpt,
-          objRadius
-        ),
+          objRadius),
         x * cpt,
-        y * cpt
-      );
+        y * cpt);
     }
   }
   return emptyCells;
