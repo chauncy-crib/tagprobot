b070fa7bf16e96300e76808e2345a13a169cab67
David Abrahams <david.abrahams@students.olin.edu>
2017-07-25 19:23:22 -0700

    removed t.plan and replaced with t.end at the end of tests

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -3,11 +3,11 @@ import * as utils from '../src/utils';
 
 
 test('test assert', t => {
-  t.plan(2);
-
   let condition = false;
   t.throws(() => { utils.assert(condition); });
 
   condition = true;
   t.doesNotThrow(() => { utils.assert(condition); });
+
+  t.end();
 });
