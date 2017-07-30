cd2df359bf4eeba3d04aa928ea221fe4eae2c77c
David Abrahams <david.abrahams@students.olin.edu>
2017-07-29 18:26:25 -0700

    adding assertArrayInBounds function and corresponding unittests

--- src/utils/asserts.js
+++ src/utils/asserts.js
@@ -5,11 +5,18 @@
  * condition: the condition, which, if false, will cause an error to be thrown
  * errorMessage: the error message to throw if condition is not true
  */
-export default function assert(condition, errorMessage = 'Assertion failed') {
+export function assert(condition, errorMessage = 'Assertion failed') {
   if (!condition) {
     if (typeof Error !== 'undefined') {
       throw new Error(errorMessage);
     }
     throw errorMessage;
   }
+}
+
+export function assertArrayInBounds(array, x, y) {
+  assert(x >= 0, 'x must be >= 0');
+  assert(y >= 0, 'y must be >= 0');
+  assert(x < array.length, 'x must be < array width');
+  assert(y < array[0].length, 'y must be < array height');
 }
