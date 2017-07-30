2ddcf2481b496309fcf58953ebb650a6025d1fb2
David Abrahams <david.abrahams@students.olin.edu>
2017-07-30 15:34:21 -0700

    separaion of drawing files

---
+++ src/draw/utils.js
@@ -0,0 +1,21 @@
+/*
+ * Iterates over each PIXI.Graphics object in sprites and adds it to the background
+ */
+export function drawSprites(sprites) {
+  sprites.forEach(sprite => {
+    tagpro.renderer.layers.background.addChild(sprite);
+  });
+}
+
+
+/*
+ * Iterates through the sprites provided to the function, and removes them from
+ * tagpro.renderer.layers.background
+ */
+export function clearSprites(sprites) {
+  if (sprites) {
+    sprites.forEach(sprite => {
+      tagpro.renderer.layers.background.removeChild(sprite);
+    });
+  }
+}
