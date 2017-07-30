a186d054b6a9b27099d7b96ef7bb38f3284ce5af
David Abrahams <david.abrahams@students.olin.edu>
2017-07-29 18:44:01 -0700

    starting this file

---
+++ test/path.spec.js
@@ -0,0 +1,8 @@
+import test from 'tape';
+import { assert, assertArrayInBounds } from '../src/utils';
+import getTarget from '../src/helpers/path';
+
+
+test('test getTarget', t => {
+  let condition = false;
+});
