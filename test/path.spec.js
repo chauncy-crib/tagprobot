de71859ba5d2c7df10c66276be3a1f9ec2ea013d
David Abrahams <david.abrahams@students.olin.edu>
2017-07-29 19:41:12 -0700

    add tests for get path

--- test/path.spec.js
+++ test/path.spec.js
@@ -1,8 +1,30 @@
 import test from 'tape';
-import { assert, assertArrayInBounds } from '../src/utils';
-import getTarget from '../src/helpers/path';
+import { getShortestPath } from '../src/helpers/path';
 
+test('test getShortestPath', t => {
+  const inputMap = [
+    [1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+  const shortestPath = getShortestPath(0, 0, 0, 2, inputMap, false);
+  t.equal(shortestPath.length, 14);
+  const shortestDiagonalPath = getShortestPath(0, 0, 0, 2, inputMap, true);
+  t.equal(shortestDiagonalPath.length, 10);
+  const impossibleMap = [
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+  const impossiblePath = getShortestPath(0, 4, 5, 5, impossibleMap, false);
+  t.notOk(impossiblePath);
 
-test('test getTarget', t => {
-  const condition = false;
+  t.throws(() => { getShortestPath(6, 3, 3, 6, impossibleMap, false); });
+  t.throws(() => { getShortestPath(2, 3, 3, 8, impossibleMap, false); });
+
+  t.end();
 });
