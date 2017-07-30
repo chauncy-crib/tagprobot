18ea317269d5c0bd6e9d792c459b838aa2be4c43
David Abrahams <david.abrahams@students.olin.edu>
2017-07-29 18:26:25 -0700

    adding assertArrayInBounds function and corresponding unittests

--- src/utils/asserts.js
+++ src/utils/asserts.js
@@ -5,7 +5,7 @@
  * condition: the condition, which, if false, will cause an error to be thrown
  * errorMessage: the error message to throw if condition is not true
  */
-export default function assert(condition, errorMessage = 'Assertion failed') {
+export function assert(condition, errorMessage = 'Assertion failed') {
   if (!condition) {
     if (typeof Error !== 'undefined') {
       throw new Error(errorMessage);
@@ -13,3 +13,11 @@ export default function assert(condition, errorMessage = 'Assertion failed') {
     throw errorMessage;
   }
 }
+
+export function assertArrayInBounds(array, x, y) {
+  assert(x >= 0, 'x must be >= 0');
+  assert(y >= 0, 'y must be >= 0');
+  assert(x < array.length, 'x must be < array width');
+  assert(y < array[0].length, 'y must be < array height');
+}
+
--- test/asserts.spec.js
+++ test/asserts.spec.js
@@ -1,5 +1,5 @@
 import test from 'tape';
-import assert from '../src/utils/asserts';
+import { assert, assertArrayInBounds } from '../src/utils/asserts';
 
 
 test('test assert', t => {
@@ -8,6 +8,18 @@ test('test assert', t => {
 
   condition = true;
   t.doesNotThrow(() => { assert(condition); });
+
+  t.end();
+});
+
+
+test('test assertArrayInBounds', t => {
+  const arr = [[0, 0, 0], [0, 0, 0]]; // a 2x3 array
+  t.throws(() => { assertArrayInBounds(arr, -1, 0); });
+  t.throws(() => { assertArrayInBounds(arr, 2, 0); });
+  t.throws(() => { assertArrayInBounds(arr, 1, -1); });
+  t.throws(() => { assertArrayInBounds(arr, 1, 4); });
+  t.doesNotThrow(() => { assertArrayInBounds(arr, 1, 1); });
 
   t.end();
 });
