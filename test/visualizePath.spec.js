8a159551a080ff4ff374459b9bfaa5160c66bf9c
David Abrahams <david.abrahams@students.olin.edu>
2017-08-04 08:51:32 -0700

    splitting tests apart

--- test/visualizePath.spec.js
+++ test/visualizePath.spec.js
@@ -47,7 +47,7 @@ test('drawPlannedPath() calls the right functions with visualsOn on', t => {
   const mockClearSprites = sinon.spy();
   const mockCreatePathSprites = sinon.spy();
   const mockDrawSprites = sinon.spy();
-  let mockVisual = sinon.stub().returns(true);
+  const mockVisual = sinon.stub().returns(true);
 
   DrawingRewireAPI.__Rewire__('clearSprites', mockClearSprites);
   DrawingRewireAPI.__Rewire__('createPathSprites', mockCreatePathSprites);
@@ -61,26 +61,43 @@ test('drawPlannedPath() calls the right functions with visualsOn on', t => {
   t.true(mockCreatePathSprites.calledWith('path', 'cpt', 'hexColor', 'alpha'));
   t.true(mockDrawSprites.calledOnce);
 
-  mockVisual = sinon.stub().returns(false);
+  DrawingRewireAPI.__ResetDependency__('clearSprites');
+  DrawingRewireAPI.__ResetDependency__('createPathSprites');
+  DrawingRewireAPI.__ResetDependency__('drawSprites');
+  DrawingRewireAPI.__ResetDependency__('visualsOn');
+  t.end();
+});
+
+test('drawPlannedPath() calls the right functions with visualsOn off', t => {
+  const mockClearSprites = sinon.spy();
+  const mockCreatePathSprites = sinon.spy();
+  const mockDrawSprites = sinon.spy();
+  const mockVisual = sinon.stub().returns(false);
+
+  DrawingRewireAPI.__Rewire__('clearSprites', mockClearSprites);
+  DrawingRewireAPI.__Rewire__('createPathSprites', mockCreatePathSprites);
+  DrawingRewireAPI.__Rewire__('drawSprites', mockDrawSprites);
   DrawingRewireAPI.__Rewire__('visualsOn', mockVisual);
+
   drawPlannedPath('path', 'cpt', 'hexColor', 'alpha');
 
   t.true(mockVisual.calledOnce);
-  t.true(mockClearSprites.calledTwice);
-  t.true(mockCreatePathSprites.calledOnce);
-  t.true(mockDrawSprites.calledTwice);
+  t.true(mockClearSprites.calledOnce);
+  t.false(mockCreatePathSprites.called);
+  t.true(mockDrawSprites.calledOnce);
 
   DrawingRewireAPI.__ResetDependency__('clearSprites');
   DrawingRewireAPI.__ResetDependency__('createPathSprites');
   DrawingRewireAPI.__ResetDependency__('drawSprites');
+  DrawingRewireAPI.__ResetDependency__('visualsOn');
   t.end();
 });
 
-test('drawNonTraversableCells() calls the right functions', t => {
+test('drawNonTraversableCells() calls the right functions with visualsOn on', t => {
   const mockClearSprites = sinon.spy();
   const mockCreateNonTraversableCellSprites = sinon.spy();
   const mockDrawSprites = sinon.spy();
-  let mockVisual = sinon.stub().returns(true);
+  const mockVisual = sinon.stub().returns(true);
 
   DrawingRewireAPI.__Rewire__('clearSprites', mockClearSprites);
   DrawingRewireAPI.__Rewire__('createNonTraversableCellSprites',
@@ -96,14 +113,31 @@ test('drawNonTraversableCells() calls the right functions', t => {
     'traversableCells', 'cpt', 'hexColor', 'alpha'));
   t.true(mockDrawSprites.calledOnce);
 
-  mockVisual = sinon.stub().returns(false);
+  DrawingRewireAPI.__ResetDependency__('clearSprites');
+  DrawingRewireAPI.__ResetDependency__('createPathSprites');
+  DrawingRewireAPI.__ResetDependency__('drawSprites');
+  DrawingRewireAPI.__ResetDependency__('visualsOn');
+  t.end();
+});
+
+test('drawNonTraversableCells() calls the right functions with visualsOn off', t => {
+  const mockClearSprites = sinon.spy();
+  const mockCreateNonTraversableCellSprites = sinon.spy();
+  const mockDrawSprites = sinon.spy();
+  const mockVisual = sinon.stub().returns(false);
+
+  DrawingRewireAPI.__Rewire__('clearSprites', mockClearSprites);
+  DrawingRewireAPI.__Rewire__('createNonTraversableCellSprites',
+    mockCreateNonTraversableCellSprites);
+  DrawingRewireAPI.__Rewire__('drawSprites', mockDrawSprites);
   DrawingRewireAPI.__Rewire__('visualsOn', mockVisual);
+
   drawNonTraversableCells('traversableCells', 'cpt', 'hexColor', 'alpha');
 
   t.true(mockVisual.calledOnce);
-  t.true(mockClearSprites.calledTwice);
-  t.true(mockCreateNonTraversableCellSprites.calledOnce);
-  t.true(mockDrawSprites.calledTwice);
+  t.true(mockClearSprites.calledOnce);
+  t.false(mockCreateNonTraversableCellSprites.called);
+  t.true(mockDrawSprites.calledOnce);
 
   DrawingRewireAPI.__ResetDependency__('clearSprites');
   DrawingRewireAPI.__ResetDependency__('createPathSprites');
