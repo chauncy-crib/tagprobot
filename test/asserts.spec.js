ce4dd227751a8d2ac46c6f765ec9588fb457951e
shanek21 <skelly.olin@gmail.com>
2017-08-05 08:38:24 -0400

    change any unit tests not conforming to the new unit test naming convention

--- test/asserts.spec.js
+++ test/asserts.spec.js
@@ -2,24 +2,36 @@ import test from 'tape';
 import { assert, assertGridInBounds } from '../src/utils/asserts';
 
 
-test('test assert', t => {
-  let condition = false;
-  t.throws(() => { assert(condition); });
-
-  condition = true;
+test('assert: does not throw errors with condition=true', t => {
+  const condition = true;
   t.doesNotThrow(() => { assert(condition); });
 
   t.end();
 });
 
 
-test('test assertGridInBounds', t => {
+test('assert: throws errors with condition=false', t => {
+  const condition = false;
+  t.throws(() => { assert(condition); });
+
+  t.end();
+});
+
+
+test('assertGridInBounds: does not throw errors with valid inputs', t => {
+  const arr = [[0, 0, 0], [0, 0, 0]]; // a 2x3 array
+  t.doesNotThrow(() => { assertGridInBounds(arr, 1, 1); });
+
+  t.end();
+});
+
+
+test('assertGridInBounds: throws errors with invalid inputs', t => {
   const arr = [[0, 0, 0], [0, 0, 0]]; // a 2x3 array
   t.throws(() => { assertGridInBounds(arr, -1, 0); });
   t.throws(() => { assertGridInBounds(arr, 2, 0); });
   t.throws(() => { assertGridInBounds(arr, 1, -1); });
   t.throws(() => { assertGridInBounds(arr, 1, 4); });
-  t.doesNotThrow(() => { assertGridInBounds(arr, 1, 1); });
 
   t.end();
 });
