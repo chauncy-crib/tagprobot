f2f71787ddfe934cffc66533c72a70856336b254
David Abrahams <david.abrahams@students.olin.edu>
2017-08-04 17:50:13 -0700

    addressing billos comments by separating tests

--- test/path.spec.js
+++ test/path.spec.js
@@ -1,7 +1,7 @@
 import test from 'tape';
 import { getShortestPath } from '../src/helpers/path';
 
-test('test getShortestPath', t => {
+test('test getShortestPath returns shortest path without diagonals', t => {
   const inputMap = [
     [1, 0, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 1, 1, 1],
@@ -10,8 +10,22 @@ test('test getShortestPath', t => {
   ];
   const shortestPath = getShortestPath(0, 0, 0, 2, inputMap, false);
   t.equal(shortestPath.length, 14);
+  t.end();
+});
+
+test('test getShortestPath returns shortest path with diagonals', t => {
+  const inputMap = [
+    [1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+  ];
   const shortestDiagonalPath = getShortestPath(0, 0, 0, 2, inputMap, true);
   t.equal(shortestDiagonalPath.length, 10);
+  t.end();
+});
+
+test('test getShortestPath returns undefined when no path exists', t => {
   const impossibleMap = [
     [1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1],
@@ -22,9 +36,19 @@ test('test getShortestPath', t => {
   ];
   const impossiblePath = getShortestPath(0, 4, 5, 5, impossibleMap, false);
   t.notOk(impossiblePath);
+  t.end();
+});
 
+test('test getShortestPath throws error when inputs are out of bounds', t => {
+  const impossibleMap = [
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+  ];
   t.throws(() => { getShortestPath(6, 3, 3, 6, impossibleMap, false); });
   t.throws(() => { getShortestPath(2, 3, 3, 8, impossibleMap, false); });
-
   t.end();
 });
