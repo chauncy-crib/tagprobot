181cc0436ad98e67708869efcd5f5c1840395a87
shanek21 <skelly.olin@gmail.com>
2017-08-12 11:31:13 -0400

    Add path planning functionality when CPTL>1

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,9 @@
-import { PPTL } from './constants';
+import { PPCL } from './constants';
 import { getMapTraversabilityInCells } from './helpers/map';
 import {
   findMyEndzone,
   findEnemyEndzone,
   findFlagStation,
-  // findEnemy, // TODO: Commenting this out until juking is implemented
   findEnemyFC,
 } from './helpers/finders';
 import { myTeamHasFlag, enemyTeamHasFlag } from './helpers/gameState';
@@ -13,6 +12,7 @@ import { getShortestPath, getTarget } from './helpers/path';
 import { move } from './utils/interface';
 import { drawPlannedPath, drawNonTraversableCells } from './draw/drawings';
 
+
 /*
  * The logic/flowchart to get where our goal is.
  *   If I have the flag, go to my endzone.
@@ -20,20 +20,15 @@ import { drawPlannedPath, drawNonTraversableCells } from './draw/drawings';
  *   If the enemy team has the flag but I can't see them, go to their endzone.
  *   If we have the flag, go to our endzone.
  *   Else, go to the flag station.
+ *
+ * @return {object} - the position, in pixels, of the bot's goal, which is
+ * determined by the current state of the game
  */
-function getGoal() {
+function getGoalPos() {
   let goal;
   const me = getMe();
   // If the bot has the flag, go to the endzone
   if (me.flag) {
-    // const chaser = findEnemy();
-    // really bad jukes! TODO: either remove or integrate this
-    // if (chaser) {
-    //   goal = chaser;
-    //   goal.x = (2 * (me.x + me.vx)) - (chaser.x + chaser.vx);
-    //   goal.y = (2 * (me.y + me.vy)) - (chaser.y + chaser.vy);
-    //   console.log('I have the flag. Fleeing enemy!');
-    // }
     goal = findMyEndzone();
     console.log('I have the flag. Seeking endzone!');
   } else {
@@ -59,48 +54,53 @@ function getGoal() {
 
 
 /*
- * Gets the x and y displacement that we should move next
+ * @return {object} - the position, in pixels, of the next immediate place to
+ * navigate to. Has an x and a y attribute.
  */
-function getSeek() {
-  const goal = getGoal();
+function getNextTargetPos() {
+  const goal = getGoalPos();
   const me = getMe();
 
-  // Version for attempting path-planning
-  const gridPosition = {
-    x: Math.floor((me.x + 20) / PPTL),
-    y: Math.floor((me.y + 20) / PPTL),
+  const myPosInCells = {
+    x: Math.floor((me.x + (PPCL / 2)) / PPCL),
+    y: Math.floor((me.y + (PPCL / 2)) / PPCL),
   };
-  const gridTarget = {
-    x: Math.floor(goal.x / PPTL),
-    y: Math.floor(goal.y / PPTL),
+  const finalTargetPosInCells = {
+    x: Math.floor(goal.x / PPCL),
+    y: Math.floor(goal.y / PPCL),
   };
+
   const traversableCells = getMapTraversabilityInCells(tagpro.map);
+
   drawNonTraversableCells(traversableCells);
+
   const shortestPath = getShortestPath(
-    gridPosition.x,
-    gridPosition.y,
-    gridTarget.x,
-    gridTarget.y,
+    myPosInCells.x,
+    myPosInCells.y,
+    finalTargetPosInCells.x,
+    finalTargetPosInCells.y,
     traversableCells,
   );
-  const seekToward = getTarget(
-    gridPosition.x,
-    gridPosition.y,
+
+  const nextTargetPosInCells = getTarget(
+    myPosInCells.x,
+    myPosInCells.y,
     shortestPath,
   );
-  seekToward.x *= PPTL;
-  seekToward.y *= PPTL;
+  const nextTargetPosInPixels = {
+    x: nextTargetPosInCells.x * PPCL,
+    y: nextTargetPosInCells.y * PPCL,
+  };
 
-  // Visualize the planned path
   drawPlannedPath(shortestPath);
 
-  // Version for not attempting path-planning
   return {
-    x: seekToward.x - (me.x + me.vx),
-    y: seekToward.y - (me.y + me.vy),
+    x: nextTargetPosInPixels.x - (me.x + me.vx),
+    y: nextTargetPosInPixels.y - (me.y + me.vy),
   };
 }
 
+
 export default function botLoop() {
-  move(getSeek());
+  move(getNextTargetPos());
 }
