58b27572a50cd122d565be9564772e76904e873a
David Abrahams <david.abrahams@students.olin.edu>
2017-08-20 17:56:44 -0400

    adding tagpro physics to constants

--- src/constants.js
+++ src/constants.js
@@ -15,7 +15,17 @@ assert(PPTL % CPTL === 0, 'CPTL does not divide evenly into PPTL');
 // Pixels per cell length
 export const PPCL = PPTL / CPTL;
 
+// drawing colors
 export const pathAlpha = 0.25;
 export const pathColor = 0x00ff00;
 export const ntAlpha = 0.4;
 export const ntColor = 0xff8c00;
+
+// tagpro physics
+const maxSpeed = 2.5; // meters per second
+const acceleration = 0.025; // meters per frame
+const tilesPerMeter = 2.5;
+const framesPerSecond = 60;
+// export in tiles per second
+export const accelTilesPerSecond = acceleration * framesPerSecond * tilesPerMeter;
+export const maxSpeedTilesPerSecond = maxSpeed * tilesPerMeter;
