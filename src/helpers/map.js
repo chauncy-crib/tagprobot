83e361284f7e7b37991c4ee5bce59bc010d0f161
shanek21 <skelly.olin@gmail.com>
2017-08-24 08:33:34 -0400

    add unit tests for findTile function

--- src/helpers/map.js
+++ src/helpers/map.js
@@ -174,28 +174,31 @@ export function getMapTraversabilityInCells(map) {
 
 
 /*
- * Returns the position x and y (in pixels) and xc, yc (in cells)
- * of the first of the specified tile types to appear starting in the
- * top left corner and moving in a page-reading fashion.
+ * Returns the position x and y (in pixels) of the first of the specified tile
+ * types to appear starting in the top left corner and moving in a page-reading
+ * fashion.
  *
  * Runtime: O(N^2)
  *
  * @param {(number | number[])} tiles - either a number representing a tileType,
  * or an array of such numbers
  */
-export function findTile(tiles) {
-  // Force an array if the input is just one tile
-  const tileArray = [].concat(tiles);
+export function findTile(map, tileIds) {
+  assert(map, 'map is undefined');
+  assert(tileIds, 'tileIds is undefined');
 
-  for (let x = 0, xl = tagpro.map.length, yl = tagpro.map[0].length; x < xl; x++) {
+  // Force an array if the input is just one tile
+  const tileIdArray = [].concat(tileIds);
+
+  for (let x = 0, xl = map.length, yl = map[0].length; x < xl; x++) {
     for (let y = 0; y < yl; y++) {
-      for (let i = 0; i < tileArray.length; i++) {
-        const tile = tileArray[i];
-        if (tagpro.map[x][y] === tile) {
-          return { x: x * PPTL, y: y * PPTL, xc: x * CPTL, yc: y * CPTL };
+      for (let i = 0; i < tileIdArray.length; i++) {
+        const tileId = tileIdArray[i];
+        if (map[x][y] === tileId) {
+          return { x: x * PPTL, y: y * PPTL };
         }
       }
     }
   }
-  throw new Error(`Unable to find tile: ${tiles}`);
+  throw new Error(`Unable to find tile: ${tileIds}`);
 }
