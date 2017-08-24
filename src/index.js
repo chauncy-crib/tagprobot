48a7fb8a0f410658f17eb21c2e91888d0064f721
shanek21 <skelly.olin@gmail.com>
2017-08-24 08:33:34 -0400

    Add unit tests for findTile function

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ function start() {
     // Call this function every time a tagpro animation frame gets drawn
     requestAnimationFrame(loop);
     if (isAutonomous()) {
-      botLoop();
+      botLoop(tagpro.map);
     }
   }
 
