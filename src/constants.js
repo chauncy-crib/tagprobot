3f084266005ddf495db051f85789efd9a32cb4e8
David Abrahams <david.abrahams@students.olin.edu>
2017-08-24 04:00:22 -0400

    replacing getTarget with getBestKeyPress

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,7 @@ export const teams = {
 export const PPTL = 40;
 
 // Cells per tile length
-export const CPTL = 1;
+export const CPTL = 2;
 assert(PPTL % CPTL === 0, 'CPTL does not divide evenly into PPTL');
 
 // Pixels per cell length
@@ -24,7 +24,7 @@ export const ntColor = 0xff8c00;
 // tagpro physics
 const maxSpeed = 2.5; // meters per second
 const acceleration = 0.025; // meters per frame
-const tilesPerMeter = 2.5;
+export const tilesPerMeter = 2.5;
 const framesPerSecond = 60;
 // export in tiles per second
 export const accelTilesPerSecond = acceleration * framesPerSecond * tilesPerMeter;
