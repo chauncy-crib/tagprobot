e93b330e8564378112a7b8ca6cd43244332a7e6b
shanek21 <skelly.olin@gmail.com>
2017-08-26 17:59:53 -0400

    Change how the tagpro map object is passed between functions

--- test/finders.spec.js
+++ test/finders.spec.js
@@ -2,11 +2,11 @@ import test from 'tape';
 import { findTile } from '../src/helpers/finders';
 import { setupTiles, teardownTiles } from './tiles.spec';
 import { getTileId } from '../src/tiles';
-import { PPTL } from '../src/constants';
+import { teams, PPTL } from '../src/constants';
 
 
 test('findTile: returns correctly with orthogonal inputs', t => {
-  setupTiles(true);
+  setupTiles(teams.BLUE);
 
   // create a dummy map from bombs, spikes, gates, and regular tiles
   const bomb = getTileId('BOMB');
@@ -16,16 +16,16 @@ test('findTile: returns correctly with orthogonal inputs', t => {
   const blank = getTileId('REGULAR_FLOOR');
 
   /* eslint-disable no-multi-spaces, array-bracket-spacing */
-  const mockMap = [
+  global.tagpro.map = [
     [bomb,    blank,    redgate],
     [redgate, bluegate, blank  ],
     [blank,   spike,    bomb   ],
   ];
   /* eslint-enable no-multi-spaces, array-bracket-spacing */
 
-  t.same(findTile(mockMap, bomb), { x: 0 * PPTL, y: 0 * PPTL });
-  t.same(findTile(mockMap, redgate), { x: 0 * PPTL, y: 2 * PPTL });
-  t.same(findTile(mockMap, spike), { x: 2 * PPTL, y: 1 * PPTL });
+  t.same(findTile(bomb), { x: 0 * PPTL, y: 0 * PPTL });
+  t.same(findTile(redgate), { x: 0 * PPTL, y: 2 * PPTL });
+  t.same(findTile(spike), { x: 2 * PPTL, y: 1 * PPTL });
 
   teardownTiles();
 
