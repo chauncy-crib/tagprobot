32e1224d3fcf9209af59e2b0f26ccce94512487c
David Abrahams <david.abrahams@students.olin.edu>
2017-09-01 01:34:30 -0400

    131: update tile API to make stored ids equivalent to tagpro map, and make user-facing functions take in an id

--- src/tiles.js
+++ src/tiles.js
@@ -3,14 +3,14 @@ import { assert } from './utils/asserts';
 import { amBlue, amRed } from './helpers/player';
 
 
-const tileInfo = {};
-const tileNames = {};
+const tileInfo = {}; // map from name to properties
+const tileNames = {}; // map from id to name
 
 /*
  * Stores all information we need about tiles in the tileInfo object, and creates the tileNames
  * object so that we can quickly map from an id to a name. This function depends on amBlue and
  * amRed, so the current player must be defined. We call this function once, after our player has an
- * id.
+ * id. All tile ids are stored as the data type they are in the tagpro api. 
  */
 export function computeTileInfo() {
   assert(_.isEmpty(tileInfo), 'tileInfo is not an empty object');
@@ -24,9 +24,9 @@ export function computeTileInfo() {
     ANGLE_WALL_4: { id: 1.4, traversable: false, permanent: true },
     REGULAR_FLOOR: { id: 2, traversable: true, permanent: true },
     RED_FLAG: { id: 3, traversable: true, permanent: false },
-    RED_FLAG_TAKEN: { id: 3.1, traversable: true, permanent: false },
+    RED_FLAG_TAKEN: { id: '3.1', traversable: true, permanent: false },
     BLUE_FLAG: { id: 4, traversable: true, permanent: false },
-    BLUE_FLAG_TAKEN: { id: 4.1, traversable: true, permanent: false },
+    BLUE_FLAG_TAKEN: { id: '4.1', traversable: true, permanent: false },
     SPEEDPAD_ACTIVE: { id: 5, traversable: false, radius: 15, permanent: false },
     SPEEDPAD_INACTIVE: { id: '5.1', traversable: true, permanent: false },
     POWERUP_SUBGROUP: { id: 6, traversable: false, radius: 15, permanent: false },
@@ -47,9 +47,9 @@ export function computeTileInfo() {
     ACTIVE_PORTAL: { id: 13, traversable: false, radius: 15, permanent: false },
     INACTIVE_PORTAL: { id: '13.1', traversable: true, permanent: false },
     SPEEDPAD_RED_ACTIVE: { id: 14, traversable: false, radius: 15, permanent: false },
-    SPEEDPAD_RED_INACTIVE: { id: 14.1, traversable: true, permanent: false },
+    SPEEDPAD_RED_INACTIVE: { id: '14.1', traversable: true, permanent: false },
     SPEEDPAD_BLUE_ACTIVE: { id: 15, traversable: false, radius: 15, permanent: false },
-    SPEEDPAD_BLUE_INACTIVE: { id: 15.1, traversable: true, permanent: false },
+    SPEEDPAD_BLUE_INACTIVE: { id: '15.1', traversable: true, permanent: false },
     YELLOW_FLAG: { id: 16, traversable: true, permanent: false },
     YELLOW_FLAG_TAKEN: { id: '16.1', traversable: true, permanent: false },
     RED_ENDZONE: { id: 17, traversable: true, permanent: true },
@@ -67,35 +67,10 @@ export function computeTileInfo() {
 
 
 /*
- * @param {number} tileID - the ID of the tile
- * @param {String} property - the name of a property
- * @return {boolean} if the corresponding tile has a value for this property
- */
-export function tileHasProperty(tileID, property) {
-  const tileIDString = String(tileID);
-  assert(_.has(tileNames, tileIDString), `Unknown tileID: ${tileID}`);
-  const tileName = tileNames[tileID];
-  return _.has(tileInfo[tileName], property);
-}
-
-
-/*
- * @param {number} tileID - the ID of the tile whose property we want
- * @param {String} property - the name of a property stored in tileInfo
- * @return the property for the input tile
- */
-export function getTileProperty(tileID, property) {
-  assert(tileHasProperty(tileID, property), `Unknown property for tile: ${tileID}, ${property}`);
-  const tileName = tileNames[tileID];
-  return tileInfo[tileName][property];
-}
-
-
-/*
  * @param {String} name - the name of a tile
- * @return the id for the input tile name
+ * @return {(number|String)} the id for the input tile name
  */
-export function getTileId(name) {
+function getTileId(name) {
   assert(_.has(tileInfo, name), `Unknown tileName: ${name}`);
   return tileInfo[name].id;
 }
@@ -104,8 +79,44 @@ export function getTileId(name) {
 /*
  * @param {number} tileID - the id for a tile
  * @param {String} name - the name of a tile
- * @return true if tileId is the id of the named tile
+ * @return true if tileId is the id of the named tile, using a type-sensative comparison
  */
 export function tileIsType(tileId, name) {
   return getTileId(name) === tileId;
 }
+
+/*
+ * @param {(number|String)} tileId - the id of a tile
+ * @return {String} the name for the input tile id
+ */
+function getTileName(tileId) {
+  assert(_.has(tileNames, tileId), `Unknown tileId: ${tileId}`);
+  const name = tileNames[tileId];
+  assert(tileIsType(tileId, name), `Input id:${tileId} was wrong data type.`);
+  return name;
+}
+
+
+/*
+ * @param {String} tileId - the id of the tile
+ * @param {String} property - the name of a property
+ * @return {boolean} if the corresponding tile has a value for this property
+ */
+export function tileHasProperty(tileId, property) {
+  const name = getTileName(tileId);
+  return _.has(tileInfo[name], property);
+}
+
+
+/*
+ * @param {String} tileId - the id of the tile whose property we want
+ * @param {String} property - the name of a property stored in tileInfo
+ * @return the property for the input tile
+ */
+export function getTileProperty(tileId, property) {
+  const name = getTileName(tileId);
+  assert(tileHasProperty(tileId, property),
+    `Tile does not have property: ${name}, ${property}`);
+  return tileInfo[name][property];
+}
+
--- test/helpers/finders.spec.js
+++ test/helpers/finders.spec.js
@@ -1,7 +1,6 @@
 import test from 'tape';
 import { findTile } from '../../src/helpers/finders';
 import { setupTiles, teardownTiles } from '../tiles.spec';
-import { getTileId } from '../../src/tiles';
 import { teams, PPTL } from '../../src/constants';
 
 
@@ -9,20 +8,21 @@ test('findTile: returns correctly with orthogonal inputs', t => {
   setupTiles(teams.BLUE);
 
   // create a dummy map from bombs, spikes, gates, and regular tiles
-  const bomb = getTileId('BOMB');
-  const spike = getTileId('SPIKE');
-  const redgate = getTileId('RED_GATE');
-  const bluegate = getTileId('BLUE_GATE');
-  const blank = getTileId('REGULAR_FLOOR');
+  const bomb = 10;
+  const spike = 7;
+  const redGate = 9.2;
+  const blueGate = 9.3;
+  const floor = 2;
 
   /* eslint-disable no-multi-spaces, array-bracket-spacing */
   global.tagpro = {
     map: [
-      [bomb,    blank,    redgate],
-      [redgate, bluegate, blank  ],
-      [blank,   spike,    bomb   ],
+      [bomb,    floor,    redGate],
+      [redGate, blueGate, floor  ],
+      [floor,   spike,    bomb   ],
     ],
   };
+
   /* eslint-enable no-multi-spaces, array-bracket-spacing */
 
   t.same(findTile('BOMB'), { x: 0 * PPTL, y: 0 * PPTL });
