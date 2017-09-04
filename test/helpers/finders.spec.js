64b6eb35b91e00565f2cbf5ac3129c3c72c437e8
David Abrahams <david.abrahams@students.olin.edu>
2017-09-04 01:35:37 -0400

    129: update finder test to not use setup tiles

--- test/helpers/finders.spec.js
+++ test/helpers/finders.spec.js
@@ -1,12 +1,10 @@
 import test from 'tape';
-import { findTile } from '../../src/helpers/finders';
-import { setupTiles, teardownTiles } from '../tiles.spec';
-import { teams, PPTL } from '../../src/constants';
+import sinon from 'sinon';
+import { findTile, __RewireAPI__ as FinderRewireAPI } from '../../src/helpers/finders';
+import { PPTL } from '../../src/constants';
 
 
 test('findTile: returns correctly with orthogonal inputs', t => {
-  setupTiles(teams.BLUE);
-
   // create a dummy map from bombs, spikes, gates, and regular tiles
   const bomb = 10;
   const spike = 7;
@@ -22,14 +20,21 @@ test('findTile: returns correctly with orthogonal inputs', t => {
       [floor,   spike,    bomb   ],
     ],
   };
-
   /* eslint-enable no-multi-spaces, array-bracket-spacing */
+
+  const mockTileHasName = sinon.stub();
+  mockTileHasName.withArgs(bomb, 'BOMB').returns(true);
+  mockTileHasName.withArgs(redGate, 'RED_GATE').returns(true);
+  mockTileHasName.withArgs(spike, 'SPIKE').returns(true);
+  mockTileHasName.returns(false);
+
+  FinderRewireAPI.__Rewire__('tileHasName', mockTileHasName);
 
   t.same(findTile('BOMB'), { x: 0 * PPTL, y: 0 * PPTL });
   t.same(findTile('RED_GATE'), { x: 0 * PPTL, y: 2 * PPTL });
   t.same(findTile('SPIKE'), { x: 2 * PPTL, y: 1 * PPTL });
 
-  teardownTiles();
+  FinderRewireAPI.__ResetDependency__('tileHasName');
 
   t.end();
 });
