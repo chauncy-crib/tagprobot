dc4bc6cc19569c4cdbca4f14f2291cbcd20b6e41
David Abrahams <david.abrahams@students.olin.edu>
2017-09-29 20:08:27 -0400

    214: add point and graph api

---
+++ src/navmesh/graph.js
@@ -0,0 +1,53 @@
+
+export class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export class Graph {
+  constructor() {
+    this.vertices = [];
+    this.edges = [];
+  }
+
+  addEdge(point1, point2) {
+    this.edges.push({ point1, point2 });
+  }
+
+  /**
+   * Return neighbors of the vertex at x, y
+   *
+   * @returns {Point[]}
+   */
+  neighbors(x, y) {
+    const res = [];
+    for (let i = 0; i < this.edges.length; i += 1) {
+      const e = this.edges[i];
+      if (e.point1.x === x && e.point1.y === y) {
+        res.push(e.point2);
+      } else if (e.point2.x === x && e.point2.y === y) {
+        res.push(e.point1);
+      }
+    }
+    return res;
+  }
+
+  addVertex(point) {
+    this.vertices.add(point);
+  }
+
+  /**
+   * Gets all vertices in the graph
+   *
+   * @returns {Point[]}
+   */
+  getVertices() {
+    return this.vertices;
+  }
+
+  getEdges() {
+    return this.edges;
+  }
+}
