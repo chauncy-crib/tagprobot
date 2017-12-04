74ba85c4398737adb8e8bb9b0ed117a8bcd5d0a0
David Abrahams <david.abrahams@students.olin.edu>
2017-12-04 15:17:04 -0500

    206: move setupTiles to its own file

---
+++ test/setupTiles.js
@@ -0,0 +1,17 @@
+import sinon from 'sinon';
+import { computeTileInfo, __RewireAPI__ as TileRewireAPI } from '../src/tiles';
+
+export function setupTiles() {
+  TileRewireAPI.__Rewire__('amBlue', sinon.stub().returns(true));
+  TileRewireAPI.__Rewire__('amRed', sinon.stub().returns(false));
+  TileRewireAPI.__Rewire__('tileInfo', {});
+  TileRewireAPI.__Rewire__('tileNames', {});
+  computeTileInfo();
+}
+
+export function teardownTiles() {
+  TileRewireAPI.__ResetDependency__('amBlue');
+  TileRewireAPI.__ResetDependency__('amRed');
+  TileRewireAPI.__ResetDependency__('tileInfo');
+  TileRewireAPI.__ResetDependency__('tileNames');
+}
