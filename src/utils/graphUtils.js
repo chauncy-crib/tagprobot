0cd8785487665946312f8dbae987faac290a9495
David Abrahams <david.abrahams@students.olin.edu>
2017-12-17 03:38:05 -0500

    206: move files and refactor out utils

--- src/utils/graphUtils.js
+++ src/utils/graphUtils.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { Point, pointsOnSameSide } from './graph';
+import { Point, pointsOnSameSide } from '../navmesh/graph';
 import { assert } from '../utils/asserts';
 
 const CLEARANCE = 19;
