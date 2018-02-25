c74e284ebabcac81e3b6ef9d6d589c932d3218e8
David Abrahams <david.abrahams@students.olin.edu>
2018-02-25 16:41:13 -0500

    328: add getEdges test

--- src/interpret/test/graph.spec.js
+++ src/interpret/test/graph.spec.js
@@ -2,6 +2,32 @@ import test from 'tape';
 import { Graph } from '../class/Graph';
 import { Point } from '../class/Point';
 
+test('getEdges', tester => {
+  tester.test('getEdges', t => {
+    const g = new Graph();
+    g.addEdgeAndVertices(new Point(0, 0), new Point(1, 0));
+    g.addEdgeAndVertices(new Point(0, 0), new Point(-1, 0));
+    g.addEdgeAndVertices(new Point(0, 0), new Point(0, -1));
+    g.addEdgeAndVertices(new Point(0, 0), new Point(0, 1));
+    t.is(g.getEdges().length, 4);
+    t.end();
+  });
+
+  tester.test('getEdges', t => {
+    const g = new Graph();
+    g.addEdgeAndVertices(new Point(2, 1), new Point(3, 2));
+    g.addEdgeAndVertices(new Point(2, 2), new Point(3, 3));
+    g.addEdgeAndVertices(new Point(-1, 1), new Point(-1, 2));
+    g.addEdgeAndVertices(new Point(-2, 1), new Point(-2, 0));
+    g.addEdgeAndVertices(new Point(-2, -1), new Point(-2, 0));
+    g.addEdgeAndVertices(new Point(0, -1), new Point(-2, -3));
+    g.addEdgeAndVertices(new Point(-4, -5), new Point(-3, -4));
+    g.addEdgeAndVertices(new Point(0, -3), new Point(3, -3));
+    t.is(g.getEdges().length, 8);
+    t.end();
+  });
+});
+
 
 test('numEdges', t => {
   const g = new Graph();
