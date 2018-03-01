c16cb847e61284c77843ca852f34cdf1f21af8f2
Shane Kelly <skelly.olin@gmail.com>
2018-03-01 16:58:50 -0500

    263: replace pointsOnSameSideOf() with Edge.isBetweenPoints()

---
+++ src/interpret/class/test/Edge.spec.js
@@ -0,0 +1,37 @@
+import test from 'tape';
+
+import { Point } from '../Point';
+import { Edge } from '../Edge';
+
+
+test('Edge.isOnSameSideOfPoints()', tester => {
+  tester.test('returns true when points are on the left side of the edge', t => {
+    const edge = new Edge(new Point(3, 0), new Point(3, 1));
+    const p1 = new Point(0, 0);
+    const p2 = new Point(0, 1);
+
+    t.false(edge.isBetweenPoints(p1, p2));
+
+    t.end();
+  });
+
+  tester.test('returns true when points are on the right side of the edge', t => {
+    const edge = new Edge(new Point(3, 0), new Point(3, 1));
+    const p1 = new Point(4, 0);
+    const p2 = new Point(4, 1);
+
+    t.false(edge.isBetweenPoints(p1, p2));
+
+    t.end();
+  });
+
+  tester.test('returns false when points are on opposite sides of the edge', t => {
+    const edge = new Edge(new Point(3, 0), new Point(3, 1));
+    const p1 = new Point(0, 0);
+    const p2 = new Point(4, 1);
+
+    t.true(edge.isBetweenPoints(p1, p2));
+
+    t.end();
+  });
+});
