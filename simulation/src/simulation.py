e145543232c5daed4ca72e46e0ac58ffa340447a
Shane Kelly <skelly.olin@gmail.com>
2018-03-25 16:23:58 -0400

    378: change simulation equations from differential equations to difference equations

--- simulation/src/simulation.py
+++ simulation/src/simulation.py
@@ -1,36 +1,8 @@
-import time
-import math
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sbs
 from matplotlib import animation
-from scipy.integrate import odeint
-
-
-def setup_time(start, stop, fs):
-    """
-    @param start - start time, in seconds
-    @param stop - stop time, in seconds
-    @param fs - sampling rate, in samples per second
-    @returns a time array
-    """
-    num_samples = (stop - start) * fs
-    time = np.linspace(start, stop, num_samples)
-
-    return time
-
-
-def derivs(x, t, b, A):
-    """
-    @param x - a vector of current state [x_pos, x_vel]
-    @param t - current time in seconds
-    @param b - damping coefficient
-    @returns derivatives of x
-    """
-    x = np.array([[x[0]], [x[1]], [x[2]], [x[3]]])
-    dxdt = A @ x
-
-    return dxdt[:,0]
+import seaborn as sbs
 
 
 def init_animation():
@@ -48,23 +20,43 @@ def animate(i):
 
 if __name__ == '__main__':
     # Calculate solution
-    fs = 30
-    time = setup_time(start=0, stop=9, fs=fs)
-    x0 = [0, 250, 0, 100] # initial condition
-    b = 0.50 # damping coefficient
-    A = np.array([[0, 1, 0, 0], [0, -b, 0, 0], [0, 0, 0, 1], [0, 0, 0, -b]])
-    sol = odeint(derivs, x0, time, args=(b, A))
-    pos_x, vel_x, pos_y, vel_y = sol.T
+    x0 = np.array([[0], [250], [0], [100]])  # initial state
+    dur = 9.0  # simulation duration, in seconds
+    dt = 0.01  # time step
+    if not (dur / dt).is_integer():
+        print('ERROR: (dur / dt) is not an integer')
+        sys.exit()
+    else:
+        samples = int(dur / dt)  # the number of samples in the simulation
+
+    b = 0.50  # damping coefficient
+    A = np.array([  # difference equations
+        [1,         dt, 0,          0],   # x = x + (dx/dt * dt)
+        [0, 1 - b * dt, 0,          0],   # dx/dt = dx/dt + (dx/dt * (-b * dt))
+        [0,          0, 1,         dt],   # y = y + (dy/dt * dt)
+        [0,          0, 0, 1 - b * dt]])  # dy/dt = dx/dt + (dy/dt * (-b * dt))
+
+    x_curr = x0
+    solution = x_curr
+    for _ in range(samples):
+        x_next = A @ x_curr
+        solution = np.append(solution, x_next, axis=1)
+        x_curr = x_next
+    pos_x, vel_x, pos_y, vel_y = solution
 
     # Plot
     sbs.set()
     fig = plt.figure(figsize=(10, 12))
 
     ax1 = fig.add_subplot(2, 1, 1)
-    ax1.plot(sol)
+    ax1.plot(pos_x)
+    ax1.plot(vel_x)
+    ax1.plot(pos_y)
+    ax1.plot(vel_y)
     ax1.set_title('State Over Time')
     ax1.set_xlabel('Time (seconds)')
-    ax1.legend(['x Position (pixels)', 'x Velocity (pixels/second)', 'y Position (pixels)', 'y Velocity (pixels/second)'])
+    ax1.legend(['x Position (pixels)', 'x Velocity (pixels/second)', 'y Position (pixels)',
+        'y Velocity (pixels/second)'])
 
     ax2 = fig.add_subplot(2, 1, 2)
     ax2.set_title('Animation')
@@ -74,7 +66,8 @@ if __name__ == '__main__':
     ax2.set_ylim(np.amin(pos_y) - 40, np.amax(pos_y) + 40)
     ax2.set_aspect('equal')
     patch = plt.Circle((0, 0), radius=19, facecolor=(1.0, 0.0, 0.0))
-    anim = animation.FuncAnimation(fig, animate, init_func=init_animation, frames=sol.shape[0], interval=1000/fs, blit=True)
-    # anim.save('../media/sim_2d.gif', dpi=80, writer='imagemagick') # saves animation as a gif
+    anim = animation.FuncAnimation(fig, animate, init_func=init_animation, frames=samples,
+        interval=1000 * dt, blit=True)
+    # anim.save('../media/sim_2d.gif', dpi=80, writer='imagemagick')  # saves animation as a gif
 
     plt.show()
