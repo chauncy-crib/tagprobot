a24855189d7e2ce0aa7be6ed74f4d069664bbdc4
Shane Kelly <shaneqkelly@gmail.com>
2018-04-24 16:34:08 -0400

    373: add tests for isMyTurnToAssumeRole

--- src/look/test/gameState.spec.js
+++ src/look/test/gameState.spec.js
@@ -1,7 +1,13 @@
 import test from 'tape';
+import sinon from 'sinon';
 
 import { ROLES } from '../constants';
-import { initMe, cleanTeammateRoles, __RewireAPI__ as GameStateRewireAPI } from '../gameState';
+import {
+  initMe,
+  cleanTeammateRoles,
+  isMyTurnToAssumeRole,
+  __RewireAPI__ as GameStateRewireAPI,
+} from '../gameState';
 
 
 test('cleanTeammateRoles()', tester => {
@@ -37,6 +43,49 @@ test('cleanTeammateRoles()', tester => {
     t.same(mockPlayerRoles, { 1: ROLES.OFFENSE, 2: ROLES.NOT_DEFINED, 3: ROLES.NOT_DEFINED });
 
     global.tagpro = undefined;
+    GameStateRewireAPI.__ResetDependency__('playerRoles');
+    t.end();
+  });
+});
+
+
+test('isMyTurnToAssumeRole()', tester => {
+  tester.test('returns true when I am oldest teammate without defined role', t => {
+    const mockCleanTeammateRoles = sinon.stub();
+    GameStateRewireAPI.__Rewire__('cleanTeammateRoles', mockCleanTeammateRoles);
+    const mockTeammatesWithLowerIds = {
+      1: { id: 1, name: 'Some Ball 1', team: 1 },
+      2: { id: 2, name: 'Some Ball 2', team: 1 },
+    };
+    const mockGetTeammatesWithLowerIds = sinon.stub().returns(mockTeammatesWithLowerIds);
+    GameStateRewireAPI.__Rewire__('getTeammatesWithLowerIds', mockGetTeammatesWithLowerIds);
+    const mockPlayerRoles = { 1: ROLES.OFFENSE, 2: ROLES.DEFENSE, 3: ROLES.NOT_DEFINED };
+    GameStateRewireAPI.__Rewire__('playerRoles', mockPlayerRoles);
+
+    t.is(isMyTurnToAssumeRole(), true);
+
+    GameStateRewireAPI.__ResetDependency__('cleanTeammateRoles');
+    GameStateRewireAPI.__ResetDependency__('getTeammatesWithLowerIds');
+    GameStateRewireAPI.__ResetDependency__('playerRoles');
+    t.end();
+  });
+
+  tester.test('returns false when an older teammate has a not defined role', t => {
+    const mockCleanTeammateRoles = sinon.stub();
+    GameStateRewireAPI.__Rewire__('cleanTeammateRoles', mockCleanTeammateRoles);
+    const mockTeammatesWithLowerIds = {
+      1: { id: 1, name: 'Some Ball 1', team: 1 },
+      2: { id: 2, name: 'Some Ball 2', team: 1 },
+    };
+    const mockGetTeammatesWithLowerIds = sinon.stub().returns(mockTeammatesWithLowerIds);
+    GameStateRewireAPI.__Rewire__('getTeammatesWithLowerIds', mockGetTeammatesWithLowerIds);
+    const mockPlayerRoles = { 1: ROLES.OFFENSE, 2: ROLES.NOT_DEFINED, 3: ROLES.NOT_DEFINED };
+    GameStateRewireAPI.__Rewire__('playerRoles', mockPlayerRoles);
+
+    t.is(isMyTurnToAssumeRole(), false);
+
+    GameStateRewireAPI.__ResetDependency__('cleanTeammateRoles');
+    GameStateRewireAPI.__ResetDependency__('getTeammatesWithLowerIds');
     GameStateRewireAPI.__ResetDependency__('playerRoles');
     t.end();
   });
