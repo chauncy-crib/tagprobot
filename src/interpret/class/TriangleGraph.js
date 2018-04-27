945e5f40110fe27cf93bbdbd6bc3b58b9e08848f
David Abrahams <david.abrahams@students.olin.edu>
2018-04-26 20:32:43 -0400

    267: enable add constraint edge, disable dynamic update for TreeNode

--- src/interpret/class/TriangleGraph.js
+++ src/interpret/class/TriangleGraph.js
@@ -526,9 +526,9 @@ export class TriangleGraph extends DrawableGraph {
    *   retriangulate around them after each addition)
    */
   dynamicUpdate(constrainedEdgesToRemove, constrainedEdgesToAdd, verticesToRemove, verticesToAdd) {
-    // _.forEach(constrainedEdgesToRemove, e => this.unfixEdge(e));
-    _.forEach(verticesToRemove, v => this.delaunayRemoveVertex(v, true));
-    _.forEach(verticesToAdd, v => this.delaunayAddVertex(v, true));
-    // _.forEach(constrainedEdgesToAdd, e => this.delaunayAddConstraintEdge(e));
+    _.forEach(constrainedEdgesToRemove, e => this.unfixEdge(e));
+    _.forEach(verticesToRemove, v => this.delaunayRemoveVertex(v, false));
+    _.forEach(verticesToAdd, v => this.delaunayAddVertex(v, false));
+    _.forEach(constrainedEdgesToAdd, e => this.delaunayAddConstraintEdge(e));
   }
 }
