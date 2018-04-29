eb93385352ea9797374b342bcdf76aadd3505a00
David Abrahams <david.abrahams@students.olin.edu>
2018-04-28 21:57:31 -0400

    267: comment out tests for dynamicupdate and removevertex, since functionality is diabled

--- src/interpret/class/test/TriangleGraph.spec.js
+++ src/interpret/class/test/TriangleGraph.spec.js
@@ -71,106 +71,106 @@ test('triangulateRegion does not create flat triangle when points are in a line'
 });
 
 
-test('delaunayRemoveVertex', tester => {
-  tester.test('removes vertex surrounded by 4 points, and validly retriangulates 4 points', t => {
-    setupPixiAndTagpro();
+// test('delaunayRemoveVertex', tester => {
+//   tester.test('removes vertex surrounded by 4 points, and validly retriangulates 4 points', t => {
+//     setupPixiAndTagpro();
 
-    const mockDTGraph = new TriangleGraph();
-    const v0 = new Point(0, 0);
-    const v1 = new Point(0, 10);
-    const v2 = new Point(10, 10);
-    const v3 = new Point(10, 0);
-    const p = new Point(3, 6);
-    mockDTGraph.addTriangle(new Triangle(p, v0, v1));
-    mockDTGraph.addTriangle(new Triangle(p, v1, v2));
-    mockDTGraph.addTriangle(new Triangle(p, v2, v3));
-    mockDTGraph.addTriangle(new Triangle(p, v3, v0));
-    mockDTGraph.delaunayRemoveVertex(p);
+//     const mockDTGraph = new TriangleGraph();
+//     const v0 = new Point(0, 0);
+//     const v1 = new Point(0, 10);
+//     const v2 = new Point(10, 10);
+//     const v3 = new Point(10, 0);
+//     const p = new Point(3, 6);
+//     mockDTGraph.addTriangle(new Triangle(p, v0, v1));
+//     mockDTGraph.addTriangle(new Triangle(p, v1, v2));
+//     mockDTGraph.addTriangle(new Triangle(p, v2, v3));
+//     mockDTGraph.addTriangle(new Triangle(p, v3, v0));
+//     mockDTGraph.delaunayRemoveVertex(p);
 
-    t.is(mockDTGraph.triangles.size, 2);
-    t.is(mockDTGraph.getVertices().length, 4);
-    t.is(mockDTGraph.getEdges().length, 5);
-    // One of the diagonals is connected
-    t.true(mockDTGraph.isConnected(v0, v2) !== mockDTGraph.isConnected(v1, v3));
+//     t.is(mockDTGraph.triangles.size, 2);
+//     t.is(mockDTGraph.getVertices().length, 4);
+//     t.is(mockDTGraph.getEdges().length, 5);
+//     // One of the diagonals is connected
+//     t.true(mockDTGraph.isConnected(v0, v2) !== mockDTGraph.isConnected(v1, v3));
 
-    resetPixiAndTagpro();
+//     resetPixiAndTagpro();
 
-    t.end();
-  });
+//     t.end();
+//   });
 
-  tester.test('removes vertex surrounded by 5 points, and validly retriangulates 5 points', t => {
-    setupPixiAndTagpro();
+//   tester.test('removes vertex surrounded by 5 points, and validly retriangulates 5 points', t => {
+//     setupPixiAndTagpro();
 
-    const mockDTGraph = new TriangleGraph();
-    const v0 = new Point(0, 0);
-    const v1 = new Point(0, 10);
-    const v2 = new Point(10, 10);
-    const v3 = new Point(10, 0);
-    const v4 = new Point(-3, 5);
-    const p = new Point(3, 6);
-    // Create a pentagon, with p in the center connected to all other vertices
-    mockDTGraph.addTriangle(new Triangle(p, v0, v4));
-    mockDTGraph.addTriangle(new Triangle(p, v4, v1));
-    mockDTGraph.addTriangle(new Triangle(p, v1, v2));
-    mockDTGraph.addTriangle(new Triangle(p, v2, v3));
-    mockDTGraph.addTriangle(new Triangle(p, v3, v0));
-    mockDTGraph.delaunayRemoveVertex(p);
+//     const mockDTGraph = new TriangleGraph();
+//     const v0 = new Point(0, 0);
+//     const v1 = new Point(0, 10);
+//     const v2 = new Point(10, 10);
+//     const v3 = new Point(10, 0);
+//     const v4 = new Point(-3, 5);
+//     const p = new Point(3, 6);
+//     // Create a pentagon, with p in the center connected to all other vertices
+//     mockDTGraph.addTriangle(new Triangle(p, v0, v4));
+//     mockDTGraph.addTriangle(new Triangle(p, v4, v1));
+//     mockDTGraph.addTriangle(new Triangle(p, v1, v2));
+//     mockDTGraph.addTriangle(new Triangle(p, v2, v3));
+//     mockDTGraph.addTriangle(new Triangle(p, v3, v0));
+//     mockDTGraph.delaunayRemoveVertex(p);
 
-    t.is(mockDTGraph.triangles.size, 3);
-    t.is(mockDTGraph.getVertices().length, 5);
-    t.is(mockDTGraph.getEdges().length, 7);
-    // Triangle on the left of the square is connected
-    t.true(mockDTGraph.isConnected(v0, v1));
-    // One of the diagonals is connected
-    t.true(mockDTGraph.isConnected(v0, v2) !== mockDTGraph.isConnected(v1, v3));
+//     t.is(mockDTGraph.triangles.size, 3);
+//     t.is(mockDTGraph.getVertices().length, 5);
+//     t.is(mockDTGraph.getEdges().length, 7);
+//     // Triangle on the left of the square is connected
+//     t.true(mockDTGraph.isConnected(v0, v1));
+//     // One of the diagonals is connected
+//     t.true(mockDTGraph.isConnected(v0, v2) !== mockDTGraph.isConnected(v1, v3));
 
-    resetPixiAndTagpro();
+//     resetPixiAndTagpro();
 
-    t.end();
-  });
-});
+//     t.end();
+//   });
+// });
 
 
-test('dynamicUpdate', tester => {
-  tester.test('single NT tile', t => {
-    setupTiles();
-    setupPixiAndTagpro();
+// test('dynamicUpdate', tester => {
+//   tester.test('single NT tile', t => {
+//     setupTiles();
+//     setupPixiAndTagpro();
 
-    const map = [
-      [2, 2, 2, 2, 2],
-      [2, 2, 2, 2, 2],
-      [2, 2, 10, 2, 2],
-      [2, 2, 2, 2, 2],
-      [2, 2, 2, 2, 2],
-    ];
-    initNavMesh(map, false);
-    const dtGraph = getDTGraph();
+//     const map = [
+//       [2, 2, 2, 2, 2],
+//       [2, 2, 2, 2, 2],
+//       [2, 2, 10, 2, 2],
+//       [2, 2, 2, 2, 2],
+//       [2, 2, 2, 2, 2],
+//     ];
+//     initNavMesh(map, false);
+//     const dtGraph = getDTGraph();
 
-    t.is(dtGraph.numFixedEdges(), 8);
-    t.is(dtGraph.numEdges(), 27);
-    t.is(dtGraph.numTriangles(), 17);
+//     t.is(dtGraph.numFixedEdges(), 8);
+//     t.is(dtGraph.numEdges(), 27);
+//     t.is(dtGraph.numTriangles(), 17);
 
-    map[2][2] = '10.1';
-    updateUnmergedGraph(getUnmergedGraph(), map, 2, 2);
-    const { unfixEdges, constrainingEdges, removeVertices, addVertices } =
-      updateMergedGraph(getMergedGraph(), getUnmergedGraph(), map, 2, 2);
-    dtGraph.dynamicUpdate(unfixEdges, constrainingEdges, removeVertices, addVertices);
+//     map[2][2] = '10.1';
+//     updateUnmergedGraph(getUnmergedGraph(), map, 2, 2);
+//     const { unfixEdges, constrainingEdges, removeVertices, addVertices } =
+//       updateMergedGraph(getMergedGraph(), getUnmergedGraph(), map, 2, 2);
+//     dtGraph.dynamicUpdate(unfixEdges, constrainingEdges, removeVertices, addVertices);
 
-    t.is(unfixEdges.length, 4);
-    t.is(constrainingEdges.length, 0);
-    t.is(removeVertices.length, 4);
-    t.is(addVertices.length, 0);
-    t.is(dtGraph.numTriangles(), 9);
-    t.is(dtGraph.numVertices(), 7);
-    t.is(dtGraph.numEdges(), 15);
-    t.is(dtGraph.numFixedEdges(), 4);
+//     t.is(unfixEdges.length, 4);
+//     t.is(constrainingEdges.length, 0);
+//     t.is(removeVertices.length, 4);
+//     t.is(addVertices.length, 0);
+//     t.is(dtGraph.numTriangles(), 9);
+//     t.is(dtGraph.numVertices(), 7);
+//     t.is(dtGraph.numEdges(), 15);
+//     t.is(dtGraph.numFixedEdges(), 4);
 
-    teardownTiles();
-    resetPixiAndTagpro();
+//     teardownTiles();
+//     resetPixiAndTagpro();
 
-    t.end();
-  });
-});
+//     t.end();
+//   });
+// });
 
 
 test('getClearancePoint()', tester => {
