1fa9e6861f5c5603b312d74bc6313b8c8371b1c2
Shane Kelly <shaneqkelly@gmail.com>
2018-05-03 21:41:50 -0400

    391: add getPGPPosition() placeholder

--- src/look/playerLocations.js
+++ src/look/playerLocations.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 
 import { playerIsOnMyTeam } from './gameState';
+import { findAllyFlagStation } from './tileLocations';
 import { Point } from '../interpret/class/Point';
 
 
@@ -21,4 +22,14 @@ export function findEnemyFC() {
 export function playerIsNearPoint(player, point, distance = 300) {
   if (!(_.has(player, 'x') && _.has(player, 'y'))) return false;
   return point.distance(new Point(player.x, player.y)) <= distance;
+}
+
+
+/**
+ * Placeholder for the get post-grab pop position function
+ * @returns {{xp: number, yp: number}} the position to be in such that when the specified player
+ *   grabs the flag, you will pop them as soon as their 0.25 seconds of invulnerability are over
+ */
+export function getPGPPosition(player) { // eslint-disable-line
+  return findAllyFlagStation();
 }
