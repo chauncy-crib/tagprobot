30a06dec3d082ada597e9b964d7180ff77931dfc
David Abrahams <david.abrahams@students.olin.edu>
2018-05-14 01:06:03 -0400

    458: move serialize matrix

--- src/cache/classes.js
+++ src/cache/classes.js
@@ -2,14 +2,20 @@ import { Point } from '../global/class/Point';
 import { Polypoint } from '../interpret/class/Polypoint';
 import { Triangle } from '../interpret/class/Triangle';
 import { Edge } from '../global/class/Edge';
+import { Matrix } from '../control/class/Matrix';
 
 
 export function deserializePoint(o) {
-  if (o.t) return new Polypoint(o.x, o.y, (new Triangle()).fromObject(o.t));
-  return (new Point()).fromObject(o);
+  if (o.t) return new Polypoint(o.x, o.y, new Triangle().fromObject(o.t));
+  return new Point().fromObject(o);
 }
 
 
 export function deserializeEdge(o) {
   return new Edge().fromObject(o);
+}
+
+
+export function deserializeMatrix(o) {
+  return new Matrix().fromObject(o);
 }
