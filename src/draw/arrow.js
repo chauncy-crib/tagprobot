a8b4cd9f7f06d089faa047dbd74f5805e08f5b1b
Shane Kelly <shaneqkelly@gmail.com>
2018-05-14 00:07:38 -0400

    466: create funcionality for drawing localGoal with arrows

---
+++ src/draw/arrow.js
@@ -0,0 +1,32 @@
+import { Point } from '../global/class/Point';
+import { Vector } from '../global/class/Vector';
+
+
+/**
+ * @param {number} x - x position of the base of the arrow shaft
+ * @param {number} y - y position of the base of the arrow shaft
+ * @param {number} dx - change in x of the arrow
+ * @param {number} dy - change in y of the arrow
+ * @param {number} [width=10] - width of the arrow head
+ * @param {number} [length=5] - length of the arrow head
+ * @returns {number[]} an array of alternating x and y values of the points that make up the polygon
+ *   of the arrow head shape (formatted like this to match preferred PIXI inputs)
+ */
+export function getArrowHead(x, y, dx, dy, width = 10, length = 10) {
+  if (dx === 0 && dy === 0) return [];
+  const base = new Point(x, y);
+  const arrow = new Vector(dx, dy);
+  const { p1, p2 } = arrow.getPerpindicularEdgeBisectedByTip(width, base);
+  const p3 = arrow.getExtensionPoint(length, base);
+  return [p1.x, p1.y, p2.x, p2.y, p3.x, p3.y];
+}
+
+
+export function drawArrow(graphics, thickness, color, alpha, x, y, dx, dy) {
+  graphics.lineStyle(thickness, color, alpha);
+  graphics.beginFill(color, alpha);
+  graphics.drawCircle(x, y, 4);
+  graphics.drawPolygon(getArrowHead(x, y, dx, dy));
+  graphics.endFill();
+  graphics.moveTo(x, y).lineTo(x + dx, y + dy);
+}
---
+++ src/draw/localGoal.js
@@ -0,0 +1,36 @@
+import { COLORS, ALPHAS, THICKNESSES } from './constants';
+import { drawArrow } from './arrow';
+
+
+let localGoalGraphics = null; // PIXI Graphics for drawing the bot's local goal
+let localGoalOn = false; // whether or not the local goal is currently being drawn
+
+
+export function drawLocalGoal(localGoal) {
+  if (!localGoalOn) return;
+  if (!localGoalGraphics) {
+    localGoalGraphics = new PIXI.Graphics();
+    tagpro.renderer.layers.background.addChild(localGoalGraphics);
+  }
+  localGoalGraphics.clear();
+  const { x, y, vx, vy } = localGoal;
+  drawArrow(
+    localGoalGraphics,
+    THICKNESSES.localGoal,
+    COLORS.localGoal,
+    ALPHAS.localGoal,
+    x,
+    y,
+    vx / 5,
+    vy / 5,
+  );
+}
+
+
+export function toggleLocalGoalVis(setTo = !localGoalOn) {
+  if (setTo === localGoalOn) return;
+  localGoalOn = setTo;
+  if (!localGoalOn) {
+    localGoalGraphics.clear();
+  }
+}
